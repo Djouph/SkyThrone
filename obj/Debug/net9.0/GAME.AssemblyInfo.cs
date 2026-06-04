f1fd1037a7cbf30e1bff1caf0b62247a730f27b3
Djouph <s218525517@ariel.ort.org.il>
2026-06-04 17:38:46 +0300

    pic

--- obj/Debug/net9.0/GAME.AssemblyInfo.cs
+++ obj/Debug/net9.0/GAME.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GAME")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f686919ce2f910a5e4f3fd722e463b44f27f28cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e3194f35573fe7a31da9ce6f6c340caeff56b19")]
 [assembly: System.Reflection.AssemblyProductAttribute("GAME")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GAME")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
